61398f7ce1ef32c627397566fec0d9c62a5963e7
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-02-06 15:08:08 +0000

    exersices in progress

--- exercises/08.2-Divide_and_conquer/app.py
+++ exercises/08.2-Divide_and_conquer/app.py
@@ -2,7 +2,18 @@ list_of_numbers = [4,	80,	85,	59,	37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76,
 
 
 #Your code here:
-
+def merge_two_list(list):
+    odd = []
+    even = []
+    merged = []
+    for num in list:
+        if num%2==0:
+            even.append(num)
+        else:
+            odd.append(num)
+    merged.append(odd)
+    merged.append(even)
+    return merged
 
 print(merge_two_list(list_of_numbers))
 
--- exercises/09-Max_integer_from_list/app.py
+++ exercises/09-Max_integer_from_list/app.py
@@ -1,3 +1,9 @@
 my_list = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34]
 
 #Your code go from here:
+max_integer = 0
+for num in my_list:
+    if num > max_integer:
+        max_integer = num
+
+print(max_integer)
